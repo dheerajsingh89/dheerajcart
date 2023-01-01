7925536df3a040e8bc6d9ac1768d297b136f0638
decodersingh <107138786+decodersingh@users.noreply.github.com>
2023-01-01 12:32:52 +0530

    message

--- dheerajcart_20_21/settings.py
+++ dheerajcart_20_21/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$#=p2f)yfjamys#5pxgkz2+nu*y8ixb$(eqyn@k*4e2920elvo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
+ALLOWED_HOSTS = ['.vercel.app','.now.com']
 
 
 # Application definition
@@ -120,7 +120,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
 STATIC_URL = 'static/'
 MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 MEDIA_URL_=("/media")
--- dheerajcart_20_21/wsgi.py
+++ dheerajcart_20_21/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dheerajcart_20_21.settings')
 
 application = get_wsgi_application()
+app=application
